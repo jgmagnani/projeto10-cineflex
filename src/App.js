123eab353005bfce861c9a033f9c0a6458e8db0a
jgmagnani <jogamabnu@gmail.com>
2022-12-12 16:30:56 -0300

    Lista de filmes ok

--- src/App.js
+++ src/App.js
@@ -4,23 +4,39 @@ import GlobalStyle from "./css/GlobalStyle"
 import MovieTimes from "./pages/MovieTimes";
 import Seats from "./pages/Seats";
 import { Assentos } from "./components/Assentos";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sucess from "./pages/Sucess";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [filmsList, setFilmsList] = useState([])
+  
+  useEffect(() => {
+		const requisicao = axios.get("https://mock-api.driven.com.br/api/v8/cineflex/movies");
 
-
+		requisicao.then(resposta => {
+			setFilmsList(resposta.data);
+    requisicao.catch("Deu erro em algo")
+		});
+	}, []);
+  console.log(filmsList)
 
   return (
     <>
       <GlobalStyle/>
       <Top/>
-      {/* <Films/> */}
-      {/* <MovieTimes/> */}
-      {/* <Seats assentos={Assentos}/> */}
-      <Sucess/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Films/>}/>
+          {/* <MovieTimes/> */}
+          {/* <Seats assentos={Assentos}/> */}
+          {/* <Sucess/> */}
+        </Routes>
+      </BrowserRouter>
+      
     </>
   );
 }
--- src/pages/Films.js
+++ src/pages/Films.js
@@ -1,31 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Imagem from "./image3.png"
 
-export default function Films(){
+
+export default function Films() {
+    const [filmsList, setFilmsList] = useState(undefined);
+
+    useEffect(() => {
+        const request = axios.get(
+            "https://mock-api.driven.com.br/api/v8/cineflex/movies"
+        );
+        request.then((resposta) => {
+            setFilmsList(resposta.data);
+        });
+        request.catch((resposta) => console.log(resposta.data));
+    }, []);
+
+    if (filmsList === undefined) {
+        return <p>L O A D I N G . . .</p>;
+    }
+
     return (
-        <>            
-            <TitlePage>            
+        <>
+            <TitlePage>
                 <h2>Selecione o filme</h2>
             </TitlePage>
+
             <FilmList>
-                <Film>
-                    <img src={Imagem}/>
-                </Film>   
-                <Film>
-                    <img src={Imagem}/>
-                </Film>
-                <Film>
-                    <img src={Imagem}/>
-                </Film>
-                <Film>
-                    <img src={Imagem}/>
-                </Film>
-                <Film>
-                    <img src={Imagem}/>
-                </Film>
-                <Film>
-                    <img src={Imagem}/>
-                </Film>             
+                {filmsList.map((movie) => (
+                    <Film key={movie.id} data-test="movie">
+                        <Link to={`/sessoes/${movie.id}`}>
+                            <img src={movie.posterURL} alt="Poster do filme" />
+                        </Link>
+                    </Film>
+                ))}               
+
             </FilmList>
         </>
     );
